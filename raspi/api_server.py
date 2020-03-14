8a64605a3dc4564c3da25290dbf1a62bea47502b
knkm3001 <1540rnkk@gmail.com>
2020-03-14 16:30:55 +0900

    ディレクトリ名変更

--- raspi/api_server.py
+++ raspi/api_server.py
@@ -1,18 +1,21 @@
 
 import json
-from flask import Flask, request, jsonify
+import requests
+from flask import Flask, request, jsonify, render_template
 from modules import funcs
 
 app = Flask(__name__)
 app.config['JSON_AS_ASCII'] = False
 
 
-
 @app.route('/')
 def home():
-    return 'APIの処理サーバだよ'
+    return 'raspiのさーばだよ'
 
-@app.route('/infrared_code/',methods=["POST"])
+
+
+
+@app.route('/api/infrared_code/',methods=["POST"])
 def infrared_code():
     post_data = request.get_json()
     funcs.exec_infrared_code(post_data)
@@ -20,4 +23,5 @@ def infrared_code():
 
 
 if __name__ == '__main__':
+    app.debug = True
     app.run(host='0.0.0.0', port=5000)
