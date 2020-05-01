629a6ad7bdcd4327ec254dd0cbd29cbd2e09371b
kazuki <1540rnkk@gmail.com>
2020-05-01 16:37:10 +0900

    fix: ラズパイのプロキシ削除

--- web-server/vue/src/web-console/vue.config.js
+++ web-server/vue/src/web-console/vue.config.js
@@ -3,10 +3,7 @@ module.exports = {
       proxy: {
         "/express/": {
           target: "http://192.168.0.24:13000",
-        },
-        "/raspi/": {
-            target: "http://192.168.0.5:5000",
-          },
+        }
       }
     }
   }
